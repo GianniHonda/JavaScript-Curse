bc9ec9bbb0b8db0212ddc1f5a2581351645813a3
Gianni Stephanie Carreira Honda <90318510+GianniHonda@users.noreply.github.com>
2024-07-16 15:15:30 +0900

    Function sum

---
+++ Aula16/funcao02.js
@@ -0,0 +1,5 @@
+function soma(n1=0, n2=0) { //se eu passar apenas um número, neste caso ele já vai considerar o outro como 0 e não vai dar mensagem de erro
+    return n1 + n2
+}
+
+console.log(soma(7))
